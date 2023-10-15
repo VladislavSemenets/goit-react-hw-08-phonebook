ef2b351286e8eab50155871544d4ea30cb9e0e10
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-10-15 14:21:44 +0300

    finished code

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { useAuth } from 'components/UseAuth';
+import { Link } from './Navigation.styled';
+
+export const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      {isLoggedIn && <Link to="/contacts">Contacts</Link>}
+    </nav>
+  );
+};
---
+++ src/components/page/Registor/Registor.js
@@ -0,0 +1,10 @@
+import { RegistorForm } from 'components/RegistorForm/RegistorForm';
+
+const Registor = () => {
+  return (
+    <div>
+      <RegistorForm />
+    </div>
+  );
+};
+export default Registor;
